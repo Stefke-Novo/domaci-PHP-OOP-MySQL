9ca895bd2a6f1cc10dbc169fcea237d2f34afc55
Stefke Velja <stef.fac150@gmail.com>
2021-11-25 12:05:24 +0100

    Pravljenje stranice za prijavu i registraciju

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super prodavnica</title>
+</head>
+<body>
+    <nav>
+        <button>Pocetna</button>
+        <button>Korpa</button>
+        <button>Proizvodi</button>
+        <button>O kompaniji</button>
+        <button>kontakt</button>
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <title>Super kupovina</title>
+
+</head>
+    <body>
+        <div id="margina"></div>
+        <img id="slika" src="https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"/>
+        <h1 id="Naslov">Dobrodošli u super kupovinu !!!<br>
+                Super prodavnica , sve za vas
+        </h1>
+        <div id="dugmici">
+        <button>
+            Prijavite se
+        </button>
+        <button>
+            Registrujte se
+        </button>
+        </div>
+    </body>
+</html>
+<?php
+?>
